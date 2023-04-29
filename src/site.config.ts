72890bb0dca11821e79645ad5eefa39b3aa0436e
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2023-04-29 19:59:07 -0300

    Basic setup (#2)

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,11 +14,11 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Juan Manuel Gentili",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "Juan's blog",
 	// Meta property used as a default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "my tiny and simple space in which I share my thoughts about everything I like to write about",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
