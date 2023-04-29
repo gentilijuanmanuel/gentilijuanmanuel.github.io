2ebd731b01bbc72b63c0eb2c5c219adb4ba3adf8
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2023-04-29 19:39:35 -0300

    GitHub Pages setup

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,7 @@ import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.astro-theme-cactus.netlify.app/",
+	site: "https://gentilijuanmanuel.github.io",
 	markdown: {
 		shikiConfig: {
 			theme: "dracula",
