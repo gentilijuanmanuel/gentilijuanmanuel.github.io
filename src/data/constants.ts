4f481ec11204c8eac00627169a833d397e535ae3
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2023-05-05 19:39:33 -0300

    Add monetization buttons

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -22,3 +22,28 @@ export const SOCIAL_LINKS = {
 export const WORK_LINKS = {
 	venmo: "https://venmo.com/",
 };
+
+export const MONETIZATION_LINKS = {
+	cafecito: {
+		url: "https://cafecito.app/jmgentili",
+		images: {
+			small: "https://cdn.cafecito.app/imgs/buttons/button_4.png",
+			medium: "https://cdn.cafecito.app/imgs/buttons/button_4_2x.png",
+			big: "https://cdn.cafecito.app/imgs/buttons/button_4_3.75x.png",
+			xBig: "https://cdn.cafecito.app/imgs/buttons/button_4.png"
+		},
+		alternativeText: "Invitame un café en Cafecito"
+	},
+	buyMeACoffee: {
+		scriptSource: "https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js",
+		name: "bmc-button",
+		slug: "jmgentili",
+		color: "#FFDD00",
+		emoji: "",
+		font: "Cookie",
+		text: "Buy me a coffee",
+		outlineColor: "#000000",
+		fontColor: "#000000",
+		coffeeColor: "#ffffff"
+	}
+};
