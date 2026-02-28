f20ef250fc68a214c11a7fe4c16c263426549858
Juan Manuel Gentili <gentilijuanmanuel@gmail.com>
2026-02-28 11:22:25 -0300

    chore: g analytics integration (#37)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/image/client" />
+
+interface ImportMetaEnv {
+	readonly PUBLIC_GA_MEASUREMENT_ID?: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
